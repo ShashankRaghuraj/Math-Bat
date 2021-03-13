ebb126472517b888a0868d18e2f7acf4d184b3da
Gabriel Wolf <gabeericwolf@gmail.com>
2021-03-13 17:13:07 -0500

    weee weeee

--- site/js/rotatescript.js
+++ site/js/rotatescript.js
@@ -1,84 +1,79 @@
-var words = (function(){
+var words = (function () {
     var words = [
-        'JavaScript',
-        'CSS3',
-        'HTML5',
-        'Ruby',
-        'Python',
-        'C++',
-        'jQuery',
-        'Google',
-        'Object C',
-        'Mysql'
-        ],
-      el = document.querySelector('.verb'),
-      currentIndex,
-      currentWord,
-      prevWord,
-      duration = 4000;
-  
-    var _getIndex = function(max, min){
-      currentIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-  
-      //Generates a random number between beginning and end of words array
-      return currentIndex;
+        "Algebra 1", "Geometry", "Algebra 2", "Precalculus", "Statistics", "Calculus", "AP Statistics", 
+        "Multivariable calculus", "Differential equations", "Linear algebra", "SAT", "LSAT", "Praxis Core", 
+        "ACT", "Biology", "Chemistry", "Physics", "Enviromental Science", "Programming", "US History", 
+        "US Government", "Politics", "World History", "Art History", "Economics", "Macroeconomics", 
+        "Microeconomics", "Careers", "Personal finance"
+    ],
+        el = document.querySelector('.verb'),
+        currentIndex,
+        currentWord,
+        prevWord,
+        duration = 4000;
+
+    var _getIndex = function (max, min) {
+        currentIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        //Generates a random number between beginning and end of words array
+        return currentIndex;
     };
-  
-    var _getWord = function(index){
-      currentWord = words[index];
-  
-      return currentWord;
+
+    var _getWord = function (index) {
+        currentWord = words[index];
+
+        return currentWord;
     };
-  
-    var _clear = function() {
-  
-      setTimeout(function(){
-        el.className = 'verb';
-      }, duration/4);
+
+    var _clear = function () {
+
+        setTimeout(function () {
+            el.className = 'verb';
+        }, duration / 4);
     };
-  
-    var _toggleWord = function(duration){
-      setInterval(function(){
-        //Stores value of previous word
-        prevWord = currentWord;
-  
-        //Generate new current word
-        currentWord = words[_getIndex(words.length-1, 0)];
-  
-        //Generate new word if prev matches current
-        if(prevWord === currentWord){
-  
-          currentWord = words[_getIndex(words.length-1, 0)];
-        }
-  
-        //Swap new value
-        el.innerHTML = currentWord;
-  
-        //Clear class styles
-        _clear();
-  
-        //Fade in word
-        el.classList.add(
-          'js-block',
-          'js-fade-in-verb'
-          );
-  
-      }, duration);
+
+    var _toggleWord = function (duration) {
+        setInterval(function () {
+            //Stores value of previous word
+            prevWord = currentWord;
+
+            //Generate new current word
+            currentWord = words[_getIndex(words.length - 1, 0)];
+
+            //Generate new word if prev matches current
+            if (prevWord === currentWord) {
+
+                currentWord = words[_getIndex(words.length - 1, 0)];
+            }
+
+            //Swap new value
+            el.innerHTML = currentWord;
+
+            //Clear class styles
+            _clear();
+
+            //Fade in word
+            el.classList.add(
+                'js-block',
+                'js-fade-in-verb'
+            );
+
+        }, duration);
     };
-  
-    var _init = function(){
-      _toggleWord(duration);
+
+    var _init = function () {
+        _toggleWord(duration);
     };
-  
+
     //Public API
     return {
-      init : function(){
-        _init();
-      }
+        init: function () {
+            _init();
+        }
     };
-  })();
-  
-  words.init();
+})();
+
+words.init();
 
 
 
