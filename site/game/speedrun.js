28f7a49fe96593e85715fc6ee7b2cfd701efc65b
Raymond Tao <21taoray@hawken.edu>
2021-03-13 20:52:30 -0500

    more work done

--- site/game/speedrun.js
+++ site/game/speedrun.js
@@ -1,34 +1,39 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getArrayRandomElement(arr) {
+    if (arr && arr.length) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
-function getArrayRandomElement (arr) {
-        if (arr && arr.length) {
-          return arr[Math.floor(Math.random() * arr.length)];
-        }
-        // The undefined will be returned if the empty array was passed
-      }
-    function getnumbers(number_num) {
-        number_of_numbers = Number(number_num);
-        let numbers = [];
-        for (let step = 0; step < number_of_numbers; step++) {
-           number = getRandomInt(9);
-           numbers.push(number);}
-        return numbers;}
-        
-    function create_solution(numbers) {
-        let operator_array = ["+", "-", "*", "/", ""];
-        let expression = "";
-        for (let item in numbers) {
-            expression += item
-            let element = getArrayRandomElement(operator_array);
-            expression += element
-        
-        }
+    // The undefined will be returned if the empty array was passed
+}
 
+function getnumbers(number_num) {
+    let number_of_numbers = Number(number_num);
+    var numbers = [];
+    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let step = 0; step < number_of_numbers; step++) {
+        var number = getRandomInt(9);
         
-
+        numbers.push(number);
     }
-
+    return numbers;
     
+}
+
+function create_solution(numbers) {
+    const operator_array = ["+", "-", "*", "/", ""];
+    let expression = "";
+    let proper_answer = "";
+    for (var item in numbers) {
+        expression += numbers[item];
+        let element = getArrayRandomElement(operator_array);
+        expression += element;
+        proper_answer += element;
+    }
+    return expression;
+
+}
+
+
--- site/game/testgame.js
+++ site/game/testgame.js
@@ -1,30 +1,117 @@
+const resize = false;
+
+function resizeCanvas() {
+    const outerCanvasContainer = $('.fabric-canvas-wrapper')[0];
+
+    const ratio = canvas.getWidth() / canvas.getHeight();
+    const containerWidth = outerCanvasContainer.clientWidth;
+    const containerHeight = outerCanvasContainer.clientHeight;
+
+    const scale = containerWidth / canvas.getWidth();
+    const zoom = canvas.getZoom() * scale;
+    canvas.setDimensions({ width: containerWidth, height: containerWidth / ratio });
+    canvas.setViewportTransform([zoom, 0, 0, zoom, 0, 0]);
+}
+
+if (resize)
+    $(window).resize(resizeCanvas);
+
 let canvas = new fabric.Canvas('myCanvas', {
     backgroundColor: 'rgb(250, 250, 250)',
+    selection: false,
 });
 
-let operators = ['+', '-', '*', '/', '(', ')']
+canvas.on('mouse:move', function (options) {
+    console.log(options.pointer.x, options.pointer.y);
+})
 
-function changeScore(new_score) {
-    score.set('text', 'Score: ' + new_score.toString());
-}
+let operators = ['+', '–', '×', '÷', '(', ')'];
+
+let x = 0;
+let y = 0;
 
 for (let i = 0; i < operators.length; i++) {
-    let op_text = new fabric.Text(operators[i], {
-        top: 300,
-        left: 50 * i,
-        lockRotation: true,
-        lockScalingX: true,
-        lockScalingY: true,
-        hasBorders: false,
-        hasControls: false,
-    });
-    op_text.on('modified', function (options) {
-        console.log('YOU FRICKING TOUCHED ME WHY');
-    })
-    canvas.add(op_text);
+    for (let j = 0; j < 20; j++) {
+        let op = new fabric.Text(operators[i], {
+            top: 300,
+            left: 100 + 100 * i,
+            lockRotation: true,
+            lockScalingX: true,
+            lockScalingY: true,
+            hasBorders: false,
+            hasControls: false,
+        });
+        op.on('mousedown', function (options) {
+            x = options.target.left;
+            y = options.target.top;
+        });
+        op.on('moved', function (options) {
+            if (!(
+                options.target.left > 50 &&
+                options.target.left < 400 &&
+                options.target.top > 75 &&
+                options.target.top < 150
+            )) {
+                options.target.left = x;
+                options.target.top = y;
+            }
+        });
+        canvas.add(op_text);
+    }
+
 }
 
-let score = new fabric.Text('Score: 0', {
+let reactants = [1, 2, 3];
+for (let i = reactants.length - 1; i >= 0; i--) {
+    canvas.add(new fabric.Text(reactants[i].toString(), {
+        top: 150,
+        left: 400 - 75 * i,
+        selectable: false,
+    }));
+}
+
+// equals sign
+canvas.add(new fabric.Text('=', {
+    top: 150,
+    left: 450,
+    selectable: false,
+}));
+
+// target value
+let target = 5;
+canvas.add(new fabric.Text(target.toString(), {
+    top: 150,
+    left: 500,
+    selectable: false,
+}));
+
+// TIME
+let elapsed = 30;
+let time = new fabric.Text('0', {
+    top: 0,
+    left: 100,
+    lockMovementX: true,
+    lockMovementY: true,
+    lockRotation: true,
+    lockScalingX: true,
+    lockScalingY: true,
+    selectable: false,
+});
+canvas.add(time);
+setInterval(function () {
+    elapsed--;
+    time.set('text', `${elapsed}`);
+    canvas.renderAll();
+}, 1000);
+
+// SCORE
+
+function changeScore(new_score) {
+    score.set('text', `${new_score}`);
+    canvas.renderAll();
+}
+
+let score = new fabric.Text('0', {
     top: 0,
     left: 500,
     lockMovementX: true,
@@ -32,17 +119,16 @@ let score = new fabric.Text('Score: 0', {
     lockRotation: true,
     lockScalingX: true,
     lockScalingY: true,
-    hasBorders: false,
-    hasControls: false,
+    selectable: false,
 });
-score.on('mousedown', function (options) {
+score.on('mousedblclick', function (options) {
     let score = prompt('yeah how much do you want bro');
     changeScore(score);
 });
 canvas.add(score);
 
-
-
+if (resize)
+    sresizeCanvas();
 
 
 
