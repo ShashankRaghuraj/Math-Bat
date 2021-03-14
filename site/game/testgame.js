c8f24d7447c35e929039492214443abce3c6a924
Raymond Tao <21taoray@hawken.edu>
2021-03-13 23:04:31 -0500

    bounding boxes

--- site/game/testgame.js
+++ site/game/testgame.js
@@ -1,100 +1,111 @@
-const resize = false;
-
-function resizeCanvas() {
-    const outerCanvasContainer = $('.fabric-canvas-wrapper')[0];
-
-    const ratio = canvas.getWidth() / canvas.getHeight();
-    const containerWidth = outerCanvasContainer.clientWidth;
-    const containerHeight = outerCanvasContainer.clientHeight;
-
-    const scale = containerWidth / canvas.getWidth();
-    const zoom = canvas.getZoom() * scale;
-    canvas.setDimensions({ width: containerWidth, height: containerWidth / ratio });
-    canvas.setViewportTransform([zoom, 0, 0, zoom, 0, 0]);
-}
-
-if (resize)
-    $(window).resize(resizeCanvas);
-
 let canvas = new fabric.Canvas('myCanvas', {
     backgroundColor: 'rgb(250, 250, 250)',
     selection: false,
 });
 
+/*
 canvas.on('mouse:move', function (options) {
     console.log(options.pointer.x, options.pointer.y);
 })
+*/
 
 let operators = ['+', '–', '×', '÷', '(', ')'];
 
 let x = 0;
 let y = 0;
 
+function within(blank, x, y)
+{
+    return x > blank.left && x < blank.left + blank.width 
+    && y > blank.top && y < blank.top + blank.height;
+}
+
 for (let i = 0; i < operators.length; i++) {
     for (let j = 0; j < 20; j++) {
         let op = new fabric.Text(operators[i], {
-            top: 300,
-            left: 100 + 100 * i,
+            top: 400,
+            left: 250 + 100 * i,
             lockRotation: true,
             lockScalingX: true,
             lockScalingY: true,
             hasBorders: false,
             hasControls: false,
+            MY_isoperator: true,
         });
         op.on('mousedown', function (options) {
             x = options.target.left;
             y = options.target.top;
         });
         op.on('moved', function (options) {
-            if (!(
-                options.target.left > 50 &&
-                options.target.left < 400 &&
-                options.target.top > 75 &&
-                options.target.top < 150
-            )) {
-                options.target.left = x;
-                options.target.top = y;
+            let t = options.target;
+            for (let i = 0; i < blanks.length; i++)
+            {
+                let blank = blanks[i];
+                if (within(blank, t.left, t.top))
+                {
+                    current_blank = blank.MY_position;
+                    break;
+                }
+                else
+                {
+                    current_blank = null;
+                }
             }
+            console.log(current_blank);
         });
         canvas.add(op);
     }
-
 }
 
-let reactants = [1, 2, 3];
-for (let i = reactants.length - 1; i >= 0; i--) {
+let current_blank = null;
+let blanks = [];
+const n_numbers = 8;
+const margin = 25;
+for (let i = 0; i < n_numbers; i++) {
+    let x_pos = 245 + 75 * i;
+    let blank = new fabric.Rect({
+        top: 260 - margin,
+        left: x_pos - margin,
+        width: 2 * margin,
+        height: 2 * margin,
+        fill: 'red',
+        selectable: false,
+        MY_position: i,
+    });
+    blanks.push(blank);
+    canvas.add(blank);
+}
+console.log(blanks);
+
+let reactants = getnumbers(n_numbers);
+for (let i = 0; i < reactants.length; i++) {
     canvas.add(new fabric.Text(reactants[i].toString(), {
-        top: 150,
-        left: 400 - 75 * i,
+        top: 240,
+        left: 200 + 75 * i,
         selectable: false,
     }));
 }
 
 // equals sign
 canvas.add(new fabric.Text('=', {
-    top: 150,
-    left: 450,
+    top: 240,
+    left: 150,
     selectable: false,
 }));
 
 // target value
 let target = 5;
 canvas.add(new fabric.Text(target.toString(), {
-    top: 150,
-    left: 500,
+    top: 240,
+    left: 100,
     selectable: false,
 }));
 
 // TIME
 let elapsed = 30;
 let time = new fabric.Text('0', {
-    top: 0,
+    top: 50,
     left: 100,
-    lockMovementX: true,
-    lockMovementY: true,
-    lockRotation: true,
-    lockScalingX: true,
-    lockScalingY: true,
     selectable: false,
 });
 canvas.add(time);
@@ -112,13 +123,8 @@ function changeScore(new_score) {
 }
 
 let score = new fabric.Text('0', {
-    top: 0,
-    left: 500,
-    lockMovementX: true,
-    lockMovementY: true,
-    lockRotation: true,
-    lockScalingX: true,
-    lockScalingY: true,
+    top: 50,
+    left: 800,
     selectable: false,
 });
 score.on('mousedblclick', function (options) {
@@ -126,18 +132,3 @@ score.on('mousedblclick', function (options) {
     changeScore(score);
 });
 canvas.add(score);
-
-if (resize)
-    sresizeCanvas();
-
-
-
-
-
-
-
-
-
-
-
-
